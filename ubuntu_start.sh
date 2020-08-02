8ced52f895d2c0390d124d2f744a5351b8e898a9
ste <bonato.ste@gmail.com>
2020-08-02 16:04:27 +0200

    Hi ubuntu

---
+++ ubuntu_start.sh
@@ -0,0 +1,3 @@
+gnome-terminal -- "ros2 launch gazebo_ros gzserver.launch.py verbose:=true factory:=true world:=empty_libstate.world"
+#gnome-terminal -- "ros2 launch thymioid_description spawn.launch name:=thymioX"
+
